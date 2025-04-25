85a1de24e2262320762ea82097486a820f9e6f95
sisay <sisaykebede5940@gmail.com>
2025-04-25 12:02:15 +0000

    title updated

--- database.sql
+++ database.sql
@@ -4,10 +4,10 @@ USE login_system;
 
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
+  username VARCHAR(20) UNIQUE NOT NULL,
+  password VARCHAR(20) NOT NULL
 );
 
 -- Example user (password = "test123")
-INSERT INTO users (username, password)
+INSERT INTO login (username, password)
 VALUES ('testuser', '$2y$10$eW5rU2v5C2LZy9UOxrkMTeFYq5r6UT0Qs/CuFc7/bsx1Sm8iEZ1Qa'); 
